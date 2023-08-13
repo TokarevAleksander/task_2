c3114fa1e24773f82d2c76956ad64086818beef8
TokarevAleksander <84794987+TokarevAleksander@users.noreply.github.com>
2023-08-13 15:21:27 +0300

    Add files via upload

---
+++ task_2.java
@@ -0,0 +1,10 @@
+public class task_2 {
+    public static void main(String[] args) {
+        double largestDouble = 1.7976931348623157E308;
+
+        byte smallestByte = Byte.MAX_VALUE;
+
+        System.out.println("Наибольшее значение типа double: " + largestDouble);
+        System.out.println("Наибольшее значение типа byte: " + smallestByte);
+    }
+}
